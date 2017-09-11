cc93a677a1ea57617d2a76d03e1af785fc4a194e
Matthew Crook <mcro591@aucklanduni.ac.nz>
2017-09-11 19:42:20 +1200

    MapLink overhaul to server command dispatch

--- src/xyz/interflare/webmaplink/commands/MapLink.java
+++ src/xyz/interflare/webmaplink/commands/MapLink.java
@@ -1,6 +1,6 @@
 package xyz.interflare.webmaplink.commands;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,25 +9,36 @@ import org.bukkit.entity.Player;
 
 public class MapLink implements CommandExecutor {
     private String _urlFormat;
+    private String _urlHost;
     
-    public MapLink(String urlFormat) {
+    public MapLink(String urlFormat, String urlHost) {
         _urlFormat = urlFormat;
+        _urlHost = urlHost;
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-	    if (sender instanceof Player) {
-	        Player p = (Player) sender;
-	        Location l = p.getLocation();
-	        
-		    sender.sendMessage(String.format(
-		            ChatColor.GREEN + "Here's your link: " +
-		            ChatColor.GRAY + _urlFormat,
-		            
-		            l.getWorld().getName(),
-		            l.getBlockX(), l.getBlockY(), l.getBlockZ()));
-	    }
-    		
-	    return true;
-	}
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            Location l = p.getLocation();
+            
+            String msg = "Click to open the web map!";
+            String link = String.format(_urlFormat,
+                    l.getWorld().getName(),
+                l.getBlockX(), l.getBlockY(), l.getBlockZ());
+            String rawMsg = String.format("{\"text\":\"[\",\"color\":\"dark_gray\",\"extra\":"
+                        + "[{\"text\":\"MapLink\",\"color\":\"aqua\",\"bold\":true},"
+                        + "{\"text\":\"] \",\"color\":\"dark_gray\"},"
+                        + "{\"text\":\"%s\",\"color\":\"gray\"}],"
+                    + "\"clickEvent\":{\"action\":\"open_url\",\"value\":\"%s\"},"
+                    + "\"hoverEvent\":{\"action\":\"show_text\",\"value\":"
+                        + "[\"\",{\"text\":\"Open Web Map\n\",\"color\":\"aqua\",\"bold\":true},"
+                            + "\"Click to browse to: %s\"]}}", msg, link, _urlHost);
+            
+           Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
+                String.format("tellraw %s %s", p.getDisplayName(), rawMsg));
+        }
+        
+        return true;
+    }
 }
